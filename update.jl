cf396e50153abf62102eae563e4f91883407bc25
connor <cbasich@cs.umass.edu>
2022-09-14 15:36:16 -0700

    Package update script.

---
+++ update.jl
@@ -0,0 +1,11 @@
+using Pkg
+
+Pkg.add("CSV")
+Pkg.add("Combinatorics")
+Pkg.add("DataFrames")
+Pkg.add("DataStructures")
+Pkg.add("DecisionTree")
+Pkg.add("JLD2")
+Pkg.add("Plots")
+Pkg.add("StatsBase")
+Pkg.add("ScikitLearnBase")
